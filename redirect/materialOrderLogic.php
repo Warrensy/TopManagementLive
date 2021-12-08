fc4e5b25fbd1d7d2dd564918d64082658a9c6000
AntiButter <if20b117@technikum-wien.at>
2021-12-08 16:37:05 +0100

    Update materialOrderLogic.php

--- redirect/materialOrderLogic.php
+++ redirect/materialOrderLogic.php
@@ -3,8 +3,9 @@
         $base = $_POST["BaseOrder"];
         $plus = $_POST["PlusOrder"];
         $max = $_POST["MaxOrder"];
+        $team = $_SESSION["Team"];
         
-        $answer = $db->addMaterials($base, $plus, $max);
+        $answer = $db->addMaterials($team, $base, $plus, $max);
 
         header('Location: index.php?site=materialOrder');
     }
