244a37309fe667ee6488cc14453dfb469ee7fce9
xrox555 <flo.le@gmx.at>
2021-12-08 18:25:27 +0100

    Update liquidFunds.php

--- redirect/liquidFunds.php
+++ redirect/liquidFunds.php
@@ -2,16 +2,15 @@
     <div class="row justify-content-center">            
         <img id="logo" src="img/Logo.png">
     </div>    
-    <div class="row justify-content-center">     
-    <?php   
-        $liquidfunds = $db->getLiquidFundsByTeamCode($_SESSION["Team"]); 
-    ?>
-    
-        <h2 id="welcome">Deine fluessigen Mittel:<?php $liquidfunds ?> </h2>
-    </div>
 
-    <div class="row"> 
-        <?php echo $liquidfunds; ?>
+
+    <div class="row justify-content-center"> 
+    <table class="table-dark table-bordered">
+        <tr>
+            <th class="tablepadding">Flüssige Mittel: </th>
+            <td class="tablepadding"><?php echo $db->getLiquidFundsByTeamCode($_SESSION["Team"]) ?></td>
+        </tr>
+    </table>
     </div>
 
 </div>
