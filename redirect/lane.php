a8f83770c2a9406604bbe5725ca7f2da6f9438cd
Bigpolshark <if20b278@technikum-wien.at>
2021-12-21 20:20:58 +0100

    asd

--- redirect/lane.php
+++ redirect/lane.php
@@ -2,22 +2,43 @@
   $lane1 = $db->loadLane($_SESSION["Team"], 1);
   if($lane1 != false)
     $prod1 = $db->getProduction($lane1["MaschinenID"]);
-
+  $money = $db->getLiquidFundsByTeamCode($_SESSION["Team"]);
 ?>
 <div class="container-fluid">
   <?php
     if($lane1 == false)
     {
-      echo "<p>Sie besitzen an dieser Position noch keine Maschine</p>";
+  ?>
+  
+      <p>Sie besitzen an dieser Position noch keine Maschine</p>
+      <p>Möchten Sie eine neue für diese Lane kaufen ?</p>
+      <form method="POST" action="index.php?site=laneLogic">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flex" id="flex" <?php if($money < 5){echo 'disabled';}?>>
+          <label class="form-check-label" for="flex">Flex-Maschine<?php if($money < 5){echo ' - nicht genug flüssige Mittel';}?></label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="conti" id="conti">
+          <label class="form-check-label" for="flex">Conti-Maschine</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="power" id="power">
+          <label class="form-check-label" for="flex">Power-Maschine</label>
+        </div>
+        <input type="submit" value="Bestellen" name="Submit">
+      </form>
+
+  <?php
+    }
+    else
+    {
+  ?>
 
 
+
+
+  <?php
     }
   ?>
 
-</div>
-
-
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">Default radio </label>
 </div>
---
+++ redirect/laneLogic.php
@@ -0,0 +1,24 @@
+<?php
+  if(isset($_POST["Submit"])){
+
+    if(isset($_POST["flex"]))
+    {
+      
+    }
+
+    /*
+    $mittel = $_POST["mittel"];
+    $base = $_POST["base"];
+    $plus = $_POST["plus"];
+    $max = $_POST["max"];
+    
+    $answer = $db->createTeam($mittel, $base, $plus, $max);
+
+
+    $_SESSION["Team"] = $answer;
+    header('Location: index.php?site=displayTeamCode');
+    */
+  }
+
+?>
+
