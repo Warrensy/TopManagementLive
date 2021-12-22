80279fc264406635d56756d955299b83e2136ef8
AntiButter <if20b117@technikum-wien.at>
2021-12-22 20:44:39 +0100

    deleted Quarter Button include

--- index.php
+++ index.php
@@ -114,10 +114,6 @@
                 }
               }
             }
-            if((isset($_GET['site']) && isset($_SESSION['Team'])) && ($_GET['site'] == 'materialOrder' || $_GET['site'] == 'rawMaterialWarehouse' || $_GET['site'] == 'production'))
-            {
-              include "redirect/quarterButton.php";
-            }
 
             ?>
         </div>
