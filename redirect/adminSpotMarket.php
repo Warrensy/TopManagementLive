2ea39197ff8597db0422601b5e7d1d89ad4ec88a
Warrensy <dorian_fetty@hotmail.de>
2022-04-04 09:33:11 +0200

    Spotmarket Feldbeschriftung

--- redirect/adminSpotMarket.php
+++ redirect/adminSpotMarket.php
@@ -1,18 +1,31 @@
 <form  method="post" action="index.php?site=adminSpotMarketLogic">
 <table>
     <tr>
-        <td>Spotmarket Anfrage hinzufügen</td>
+        <td class="text-center"><b>Hier können Sie neue Spotmarket Anfragen erstellen</b></td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td class="text-center">Wählen Sie eine Produktart aus</td>
     </tr>
     <tr>
-        <td><select name="material" id="material">
+        <td class="text-center "><select class="w-25 p-3 h-25 d-inline-block" name="material" id="material">
     <option value="Base">Base</option>
     <option value="Plus">Plus</option>
     <option value="Max">Max</option>
     </select></td>
     </tr>
+    <tr><td><br></td></tr>
     <tr>
-        <td><input type="number" id="menge" name="menge"></td>
+        <td class="text-center">Geben Sie die geforderte Menge ein</td>
     </tr>
-    <input class="col btn btn-success"   type="submit" value="Anfrage Speichern" name ="save" ID = "save">
+    <tr>
+        <td class="text-center"><input class="w-25 p-3" type="number" id="menge" name="menge"></td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td>
+            <input class="col btn btn-success"   type="submit" value="Anfrage Speichern" name="save" ID= "save">
+        </td>
+    </tr>
 </table>
 </form>
