f3ea96db3189d2bd51077fc56f02b40d9626e90c
johannesrathgeb <jrathgeb2001@gmail.com>
2022-05-31 17:56:01 +0200

    Gewinn und Verlustrechnung Design

---
+++ redirect/profitAndLoss.php
@@ -0,0 +1,101 @@
+<div class="container-fluid">
+<div class="row background-white" style="font-weight: bold; padding:1.5%;">Gewinn- und Verlustrechung</div>    
+<div class="row border-only-bottom background-white">
+        <div class="col-3"></div>
+        <div class="col-1"></div>
+        <div class="col-1" style="font-weight: bold; text-align: center;">Jahr 0</div>
+        <div class="col-1"></div>
+        <div class="col-1" style="font-weight: bold; text-align: center;">Jahr 1</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 profitLossStyle">Umsatzerlöse</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">61</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 border-only-bottom profitLossStyle">Herstellungskosten</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(24)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 background-yellow profitLossStyle">Bruttoergebnis</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">=</div>
+        <div class="col-1 background-yellow profitLossStyle" style="text-align: center;">37</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 background-yellow profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 profitLossStyle">Forschung & Entwicklung</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(4)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 profitLossStyle">Verwaltung</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(8)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 profitLossStyle">Marketing & Vertrieb</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(4)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 profitLossStyle">Sonstige betriebliche Erträge</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">+</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">0</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 border-only-bottom profitLossStyle">Abschreibung</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(12)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 background-yellow profitLossStyle">Betriebsergebnis (EBIT)</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">=</div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">9</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 border-only-bottom profitLossStyle">Zinsen</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(3)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 background-yellow profitLossStyle">Ergebnis vor Steuern (EBIT)</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">=</div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">6</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 border-only-bottom profitLossStyle">Steuern vom Ergebnis (1/3)</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">-</div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">(2)</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+    <div class="row background-white">
+        <div class="col-3 background-yellow profitLossStyle">Ergebnis nach Steuern (EAT)</div>
+        <div class="col-1 profitLossStyle" style="font-weight: bold; text-align: center;">=</div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">4</div>
+        <div class="col-1 profitLossStyle"></div>
+        <div class="col-1 background-yellow border-only-bottom profitLossStyle" style="text-align: center;">75</div>
+    </div>
+</div>
